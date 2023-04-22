c3f413ff345dda5625ec5d931f1cc9c9857902d3
musnow <ezplayingd@126.com>
2023-04-22 08:37:01 +0800

    refactor(main): using _log replace print

--- spbot.py
+++ spbot.py
@@ -24,6 +24,10 @@ async def botmarket():
     headers = {'uuid':'cbc11a5f-609f-4274-8656-765c5f96e19b'}
     async with aiohttp.ClientSession() as session:
         await session.post(api, headers=headers)
+# 保存文件
+@bot.task.add_interval(minutes=5)
+async def save_file_task():
+    await save_all_files()
 
 ######################################################################################
 
@@ -34,9 +38,7 @@ async def hello(msg:Message,*arg):
     try:
         await msg.reply(f"hello world!")
     except Exception as result:
-        await BaseException_Handler("hh",traceback.format_exc(),msg,bot)
-        err_str = f"ERR! [{getTime()}] hh\n```\n{traceback.format_exc()}\n```"
-        await bot.client.send(debug_ch, err_str)#发送错误信息到指定频道
+        await BaseException_Handler("hh",traceback.format_exc(),msg,debug_ch)
         
 @bot.command(name="sphelp")
 async def help(msg:Message,*arg):
@@ -49,16 +51,14 @@ async def help(msg:Message,*arg):
         text+="设置完成后，bot会每30m获取最新的助力者id，并在服务器内发送感谢信息"
         text+="`/spr-d` 取消助力者提醒\n"
         cm = CardMessage()
-        c = Card(Module.Header(f"本bot支持的命令如下"),Module.Context(Element.Text("由MOAR#7134开发，开源代码见 [Github](https://github.com/Aewait/Kook-SponsorRole-Bot)",Types.Text.KMD)),Module.Divider())
+        c = Card(Module.Header(f"本bot支持的命令如下"),Module.Context(Element.Text("开源代码见 [Github](https://github.com/musnwos/Kook-SponsorRole-Bot)",Types.Text.KMD)),Module.Divider())
         c.append(Module.Section(Element.Text(text, Types.Text.KMD)))
         c.append(Module.Divider())
         c.append(Module.Section('有任何问题，请加入帮助服务器与我联系', Element.Button('帮助', 'https://kook.top/gpbTwZ', Types.Click.LINK)))
         cm.append(c)
         await msg.reply(cm)
     except Exception as result:
-        await BaseException_Handler("sphelp",traceback.format_exc(),msg,bot)
-        err_str = f"ERR! [{getTime()}] sphelp\n```\n{traceback.format_exc()}\n```"
-        await bot.client.send(debug_ch, err_str)#发送错误信息到指定频道
+        await BaseException_Handler("sphep",traceback.format_exc(),msg,debug_ch)
 
 ##################################################################################################
 
@@ -80,7 +80,7 @@ def check_sponsor(SpDict:dict,it: dict,guild_id:str):
 async def spr_delete(msg:Message,*arg):
     if not logging(msg):
         await msg.reply(f"当前命令需要在公共服务器内使用！")
-        print(f"[PrivateMessage] Au:{msg.author_id} inform reply, return")
+        _log.info(f"[PrivateMessage] Au:{msg.author_id} inform reply, return")
         return
     try:
         global SponsorDict
@@ -94,10 +94,7 @@ async def spr_delete(msg:Message,*arg):
         del SponsorDict['guild'][guild_id]
         # 回复
         await msg.reply(f"助力者提醒删除成功")
-        # 保存到文件
-        with open("./log/GuildLog.json", 'w', encoding='utf-8') as fw2:
-            json.dump(SponsorDict, fw2, indent=2, sort_keys=True, ensure_ascii=False)
-        print(f"[spr-d] G:{guild_id} Au:{msg.author_id} move to del_guild")
+        _log.info(f"[spr-d] G:{guild_id} Au:{msg.author_id} move to del_guild")
     except Exception as result:
         await BaseException_Handler("spr-d",traceback.format_exc(),msg,bot)
         err_str = f"ERR! [{getTime()}] spr-d\n```\n{traceback.format_exc()}\n```"
@@ -108,7 +105,7 @@ async def spr_delete(msg:Message,*arg):
 async def spr_set(msg:Message,role_id:str="err",ch_id:str="err",*arg):
     if not logging(msg):
         await msg.reply(f"当前命令需要在公共服务器内使用！")
-        print(f"[PrivateMessage] Au:{msg.author_id} inform reply, return")
+        _log.info(f"[PrivateMessage] Au:{msg.author_id} inform reply, return")
         return
     elif role_id == "err":
         await msg.reply(f"您没有提供助力者角色id！获取方式：\n```\n个人设置-高级设置-打开开发者模式，进入服务器设置-角色设置-右键助力者角色-复制id\n```\n文字频道id获取方式同上，打开开发者模式后，右键文字频道-复制id即可")
@@ -146,79 +143,79 @@ async def spr_set(msg:Message,role_id:str="err",ch_id:str="err",*arg):
         else:
             text_reply = f"设置成功!第一波感谢信息已送出~\n频道：{ch.name}\n频道id：{ch_id}\n助力者角色id：{role_id}"
         await msg.reply(text_reply)
-        # 保存到文件
-        with open("./log/GuildLog.json", 'w', encoding='utf-8') as fw2:
-            json.dump(SponsorDict, fw2, indent=2, sort_keys=True, ensure_ascii=False)
-        print(f"[spr] G:{guild_id} C:{ch_id} Au:{msg.author_id} R:{role_id} set success")
+
+        await save_all_files()
+        _log.info(f"[spr] G:{guild_id} C:{ch_id} Au:{msg.author_id} R:{role_id} set success")
     except Exception as result:
         await BaseException_Handler("spr",traceback.format_exc(),msg,bot)
         err_str = f"ERR! [{getTime()}] spr\n```\n{traceback.format_exc()}\n```"
         await bot.client.send(debug_ch, err_str)#发送错误信息到指定频道
-            
+
+async def del_guild_log(guild_id:str):
+    """删除出错服务器"""
+    global SponsorDict
+    SponsorDict['err_guild'][guild_id] = SponsorDict['guild'][guild_id]
+    del SponsorDict['data'][guild_id]
+    del SponsorDict['guild'][guild_id]
+
 # 感谢助力者（每1h检查一次）
 @bot.task.add_interval(minutes=30)
 async def thanks_sponser():
     try:
         global SponsorDict
-        print(f"[BOT.THX.TASK] start at {getTime()}")
+        _log.info(f"[BOT.THX.TASK] start at {getTime()}")
         TempDict = deepcopy(SponsorDict)
-        for guild_id in SponsorDict['guild']:
+        for guild_id in TempDict['guild']:
             try:
-                ret = await fetch_role_list(guild_id,SponsorDict['guild'][guild_id]['role_id'])
+                ret = await fetch_role_list(guild_id,TempDict['guild'][guild_id]['role_id'])
+                # 机器人不在服务器，直接删除键值
                 if ('该用户不在该服务器内' in ret['message']) or ret['code']!=0:
-                    log_str = f"ERR! [BOT.THX.TASK] {ret}\n"
-                    log_str +=f"[BOT.THX.TASK] del {guild_id}"
+                    log_str = f"ERR! [BOT.THX.TASK] {ret}\n[BOT.THX.TASK] del {guild_id}"
                     # 赋值后删除（留档）
-                    TempDict['err_guild'][guild_id] = deepcopy(TempDict['guild'][guild_id])
-                    del TempDict['data'][guild_id]
-                    del TempDict['guild'][guild_id]
-                    print(log_str)
+                    await del_guild_log(guild_id)
+                    _log.info(log_str)
                     continue
                 # 用户数量相同，无需更新
-                sz = len(SponsorDict['data'][guild_id])
+                sz = len(TempDict['data'][guild_id])
                 if ret['data']['meta']['total'] == sz:
-                    print(f"[BOT.THX.TASK] G:{guild_id} No New_Sp, same_len [{sz}]")
+                    _log.info(f"[BOT.THX.TASK] G:{guild_id} No New_Sp, same_len [{sz}]")
                     continue
                 # 用户数量不同，遍历检查
                 log_text = f"[BOT.THX.TASK] {getTime()} G:{guild_id} NewSp:"
                 send_text = ""
-                channel = await bot.client.fetch_public_channel(SponsorDict['guild'][guild_id]['channel_id'])#发送感谢信息的文字频道
+                channel = await bot.client.fetch_public_channel(TempDict['guild'][guild_id]['channel_id'])#发送感谢信息的文字频道
                 for its in ret['data']['items']:
-                    if check_sponsor(TempDict,its,guild_id):
+                    if check_sponsor(SponsorDict,its,guild_id):
                         send_text+= f"感谢 (met){its['id']}(met) 对本服务器的助力\n"
                         log_text += f"({its['id']}_{its['username']}#{its['identify_num']}) "
                 if send_text!="":
                     await bot.client.send(channel, send_text)
-                print(log_text)
+                _log.info(log_text)
             except Exception as result:
-                err_str = f"ERR! [{getTime()}] TASK.G:{guild_id}\n```\n{traceback.format_exc()}\n```"
+                _log.exception(f"Err [BOT.THX.TASK] G:{guild_id}")
                 # 赋值后删除（留档）
-                TempDict['err_guild'][guild_id] = deepcopy(TempDict['guild'][guild_id])
-                del TempDict['data'][guild_id]
-                del TempDict['guild'][guild_id]
-                print(err_str)
+                await del_guild_log(guild_id)
+                err_str = f"ERR! [{getTime()}] [BOT.THX.TASK] G:{guild_id}\n```\n{traceback.format_exc()}\n```"
                 await bot.client.send(debug_ch, err_str)#发送错误信息到指定频道
         
         # 保存到文件
-        SponsorDict = TempDict
-        with open("./log/GuildLog.json", 'w', encoding='utf-8') as fw2:
-            json.dump(SponsorDict, fw2, indent=2, sort_keys=True, ensure_ascii=False)
-        print(f"[BOT.THX.TASK] finish at {getTime()}")
+        await save_all_files()
+        _log.info(f"[BOT.THX.TASK] finish at {getTime()}")
     except Exception as result:
+        _log.exception("Err in [BOT.THX.TASK]")
         err_str = f"ERR! [{getTime()}] [BOT.THX.TASK]\n```\n{traceback.format_exc()}\n```"
-        print(err_str)
         await bot.client.send(debug_ch, err_str)#发送错误信息到指定频道
 
 
 # 开机获取debug频道
-@bot.task.add_date()
-async def loading_channel():
+@bot.on_startup
+async def loading_channel(b:Bot):
     try:
         global debug_ch
         debug_ch = await bot.client.fetch_public_channel(config['debug_ch'])
-        print("[BOT.START] fetch_public_channel success")
+        _log.info("[BOT.START] fetch_public_channel success")
     except:
-        print("[BOT.START] fetch_public_channel failed")
+        _log.critical("[BOT.START] fetch_public_channel failed")
         os._exit(-1)  #出现错误直接退出程序
         
 if __name__ == '__main__':
